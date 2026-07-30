f4b105ccbb461628fba5d4a9fd930c7203e8acab
Abby <alimpioso@gmail.com>
2026-07-30 04:59:42 -0400

    tried adding vite plugin for netlify

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import netlify from "@netlify/vite-plugin";
 
 const clientDirectory = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: clientDirectory,
-  plugins: [react()],
+  plugins: [react(), netlify()],
   server: {
     port: 5173,
     proxy: {
